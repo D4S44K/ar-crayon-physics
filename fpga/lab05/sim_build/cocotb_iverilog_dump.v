293bd4429096839a245cf08190302acb088ad8a2
Younghun Roh <yhunroh@gmail.com>
2024-12-11 15:11:01 -0500

    Removing unnecessary files

--- fpga/lab05/sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("/Users/danielvargas/Desktop/MIT/6.205/lab05/sim_build/center_of_mass.fst");
-    $dumpvars(0, center_of_mass);
-end
-endmodule
